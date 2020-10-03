ec832e45b0b6478fec4377d50cbd86d239fb34cc
ipuppet <zimingw.ol@outlook.com>
2020-10-03 22:40:58 +0800

    新增备份功能

--- scripts/ui/main/setting.js
+++ scripts/ui/main/setting.js
@@ -3,6 +3,10 @@ const BaseUISetting = require("/scripts/ui/components/base-ui-setting")
 class SettingUI extends BaseUISetting {
     constructor(kernel, factory) {
         super(kernel, factory)
+        this.backupPath = "drive://SubStore/backup.json"
+        if (!$file.exists("drive://SubStore/")) {
+            $file.mkdir("drive://SubStore/")
+        }
     }
 
     readme() {
@@ -17,8 +21,9 @@ class SettingUI extends BaseUISetting {
     }
 
     clearCache() {
+        this.start()
         require("./home").clearCache()
-        $ui.toast($l10n("FINISH"))
+        this.done()
     }
 
     tips() {
@@ -27,6 +32,98 @@ class SettingUI extends BaseUISetting {
             message: `运行环境中的QX即Quantumult X
 其他则是指Loon和Surge
 当切换运行环境时，请清除缓存`
+        })
+    }
+
+    backupToICloud() {
+        this.start()
+        const backupAction = () => {
+            $http.get({
+                url: `${this.kernel.host}/api/storage`,
+                handler: (resp) => {
+                    if (resp.error) {
+                        $ui.alert($l10n("BACKUP_ERROR"))
+                        this.cancel()
+                    } else {
+                        $file.write({
+                            data: $data({ string: JSON.stringify(resp.data) }),
+                            path: this.backupPath
+                        })
+                        this.done()
+                    }
+                }
+            })
+        }
+        if ($file.exists(this.backupPath)) {
+            $ui.alert({
+                title: $l10n("BACKUP"),
+                message: $l10n("ALREADY_HAS_BACKUP"),
+                actions: [
+                    {
+                        title: $l10n("OK"),
+                        handler: () => {
+                            backupAction()
+                        }
+                    },
+                    {
+                        title: $l10n("CANCEL"),
+                        handler: () => { this.cancel() }
+                    }
+                ]
+            })
+        } else {
+            backupAction()
+        }
+    }
+
+    recoverFromICloud() {
+        this.start()
+        $drive.open({
+            handler: data => {
+                let message = "您要恢复的内容包含以下信息，是否恢复？"
+                try {
+                    let config = JSON.parse(data.string)
+                    let subsList = Object.keys(config.subs)
+                    let collectionsList = Object.keys(config.collections)
+                    message += "\n单个订阅：\n"
+                    message += subsList.join("\n")
+                    message += "\n组合订阅：\n"
+                    message += collectionsList.join("\n")
+                } catch (error) {
+                    $ui.alert("无法读取到正确的信息!")
+                    return
+                }
+                $ui.alert({
+                    title: $l10n("ALERT_INFO"),
+                    message: message,
+                    actions: [
+                        {
+                            title: $l10n("OK"),
+                            handler: () => {
+                                $http.post({
+                                    url: `${this.kernel.host}/api/storage`,
+                                    body: data.string,
+                                    handler: (resp) => {
+                                        if (resp.error) {
+                                            $ui.alert($l10n("RECOVER_ERROR"))
+                                            this.cancel()
+                                        } else {
+                                            // 完成动画
+                                            this.done()
+                                        }
+                                    }
+                                })
+                            }
+                        },
+                        {
+                            title: $l10n("CANCEL"),
+                            handler: () => {
+                                this.cancel()
+                            }
+                        }
+                    ]
+                })
+            }
         })
     }
 }
