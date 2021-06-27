5539c034e426db28fda2ddb194d95e1cb74541b2
子明 <37722993+ipuppet@users.noreply.github.com>
2021-06-27 23:51:31 +0800

    Update

--- scripts/app.js
+++ scripts/app.js
@@ -5,6 +5,7 @@ class AppKernel extends Kernel {
         super()
         this.settingComponent = this.registerComponent("Setting")
         this.setting = this.settingComponent.controller
+        this.setting.setLargeTitle(false)
         this.initSettingMethods()
         this.loading = this.registerComponent("Loading").controller
         // 备份路径
